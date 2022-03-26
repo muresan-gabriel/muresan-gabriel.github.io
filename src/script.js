b4d84254d94a7ae7c8ec2dc9ac09d66d8f90f7b8
Gabriel Mureșan <32938828+gabeegabee@users.noreply.github.com>
2022-03-26 13:01:45 +0200

    Changes

--- src/script.js
+++ src/script.js
@@ -1,10 +1,5 @@
 document.addEventListener("contextmenu", (event) => event.preventDefault());
 
-var cursor = document.getElementById("cursor");
-document.body.addEventListener("mousemove", function (e) {
-  (cursor.style.left = e.clientX + "px"), (cursor.style.top = e.clientY + "px");
-});
-
 window.scroll({
   top: 0,
   left: 0,
