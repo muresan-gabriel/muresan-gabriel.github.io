1296a6cf45d13a8d370f6365bb7f726224ca56d8
Gabriel Mureșan <32938828+gabeegabee@users.noreply.github.com>
2022-03-27 16:05:20 +0300

    Mobile version update

--- src/script.js
+++ src/script.js
@@ -101,33 +101,3 @@ window.onload = function () {
   css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
   document.body.appendChild(css);
 };
-
-document.addEventListener("DOMContentLoaded", function () {
-  window.addEventListener("scroll", function () {
-    if (window.scrollY > 50) {
-      document.getElementById("navExplore").classList.add("sticky-top");
-      // add padding top to show content behind navbar
-      document.getElementById("navExplore").style.cssText +=
-        "backdrop-filter: blur(15px) saturate(160%) contrast(45%) brightness(90%);border-bottom-left-radius: 25px;border-bottom-right-radius: 25px;box-shadow: 10px 5px 20px #000000 0.3;";
-    } else {
-      document.getElementById("navExplore").classList.remove("sticky-top");
-      // remove padding top from body
-      document.body.style.paddingTop = "0";
-    }
-  });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  window.addEventListener("scroll", function () {
-    if (window.scrollY < 50) {
-      document.getElementById("navExplore").classList.remove("sticky-top");
-      // add padding top to show content behind navbar
-      document.getElementById("navExplore").style.cssText -=
-        "backdrop-filter: blur(15px) saturate(160%) contrast(45%) brightness(90%);border-bottom-left-radius: 25px;border-bottom-right-radius: 25px;box-shadow: 10px 5px 20px #000000 0.3;";
-    } else {
-      document.getElementById("navExplore").classList.add("sticky-top");
-      // remove padding top from body
-      document.body.style.paddingTop = "0";
-    }
-  });
-});
